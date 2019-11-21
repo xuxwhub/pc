be3fc706238ac22135929dc64e7cdb02430c650f
xuxw <xuxinwei@zhengheyinshi.com>
2019-11-21 14:33:46 +0800

    addTestFlag

--- src/main/java/com/dunshan/biz/BaseApplication.java
+++ src/main/java/com/dunshan/biz/BaseApplication.java
@@ -5,13 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
-@SpringBootApplication(scanBasePackages = {"com.dunshan.biz"})
+@SpringBootApplication(scanBasePackages = {"com.dunshan"})
 @MapperScan(basePackages = {"com.dunshan.biz.mapper"})
 @EnableEurekaClient
 @EnableFeignClients(basePackageClasses = {PDClient.class})
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 public class BaseApplication {
 
   public static void main(String[] args) {
